cefaa4aaf3f9e487b1aec78f96b57d0a299d6034
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-09-24 11:39:36 +0100

    LinkedList

--- TheQueues.java
+++ TheQueues.java
@@ -1,8 +1,20 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class TheQueues {
     public static void main(String[] args) {
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Olakunle", 32));
+        linkedList.add(new Person("Qalbi", 28));
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext()) {
+            System.out.println(personListIterator.next());
+        }
+
+    }
+
+    private static void queues() {
         Queue<Person> supermarket = new LinkedList<>();
         supermarket.add(new Person("Olakunle", 32));
         supermarket.add(new Person("Ade", 31));
@@ -15,6 +27,5 @@ public class TheQueues {
         System.out.println(supermarket.peek());
     }
 
-    static record Person(String name, int age) {
-    }
+    static record Person(String name, int age) {}
 }
